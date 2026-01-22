b8e916112521c7a2802d3f36e783766f654a1e82
heyclore <cloore@gmail.com>
2026-01-22 12:46:11 +0700

    m

--- bash/apps-bin/toggle-multimedia-sysext.sh
+++ bash/apps-bin/toggle-multimedia-sysext.sh
@@ -1,8 +1,22 @@
-if systemd-sysext | grep 'multimedia'; then
+#!/usr/bin/env bash
+
+# Prepare extensions directory
+# Prepare multimedia symlink
+if [[ ! -d /run/extensions ]]; then
+  echo "Creating /run/extensions..."
+  sudo mkdir -p /run/extensions
+  sudo ln -s "/home/noodle/apps/pac/multimedia/" /run/extensions/multimedia
+else
+  echo "/run/extensions already exists, skipping."
+fi
+
+# Toggle systemd-sysext overlay
+if systemd-sysext | grep -q 'multimedia'; then
   echo "Multimedia overlay is merged. Unmerging..."
   sudo systemd-sysext unmerge
 else
   echo "Multimedia overlay is not merged. Merging..."
-  sudo systemd-sysext merge
+  #sudo systemd-sysext merge
+  sudo systemd-sysext refresh
 fi
 
