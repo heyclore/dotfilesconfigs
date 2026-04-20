d230328018e47b1f0008c0d559acfc7a8905b152
heyclore <cloore@gmail.com>
2026-04-20 19:33:42 +0700

    m

---
+++ bash/init_dirs.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p apps/{aur/{brave-bin,google-chrome,upwork},git/{boomer,dmenu,dotfilesconfigs,dwm,neofetch,st,xfe},opt/{bin,include,lib,share},pac/extra/usr/{bin,etc,include,lib,share},src/{google-cloud-cli-linux-x86_64,katalon},sys/{devcore,multimedia}}
+
+echo "Directory structure created successfully."
